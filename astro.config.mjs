89899e593ae36d12f7cf7213e29b7e1a66e09745
KAOS <yosoycedric@gmail.com>
2025-03-04 16:18:11 -0600

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soycedric.github.io',
-  outDir: './dist', // Asegúrate de que esto sea './dist'
   trailingSlash: 'always',
   build: {
     assets: '_assets'
